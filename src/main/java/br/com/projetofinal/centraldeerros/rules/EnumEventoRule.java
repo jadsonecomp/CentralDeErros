d952783074d363843fbe72c909610ccdde17c481
jadsonecomp <jadsonecomp@gmail.com>
2020-08-24 11:30:28 -0300

    Commit dos rules do projeto

---
+++ src/main/java/br/com/projetofinal/centraldeerros/rules/EnumEventoRule.java
@@ -0,0 +1,30 @@
+package br.com.projetofinal.centraldeerros.rules;
+
+import br.com.projetofinal.centraldeerros.entity.Evento;
+import br.com.projetofinal.centraldeerros.enums.Level;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class EnumEventoRule implements Specification<Evento> {
+
+    private String colunaNome;
+    private String colunaValor;
+
+    public EnumEventoRule(String name, String value) {
+        this.colunaNome = name;
+        this.colunaValor = value;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Evento> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        if (this.colunaValor == null) {
+            return criteriaBuilder.isTrue(criteriaBuilder.literal(true));
+        }
+        return criteriaBuilder.equal(root.get(this.colunaNome), Level.find(this.colunaValor));
+    }
+
+}
